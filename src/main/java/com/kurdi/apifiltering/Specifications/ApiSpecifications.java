349b773fac7e7a7ad784378d2fa7d2466ca928b7
sheriff-kurdi <sheriff.kurdi@gmail.com>
2023-07-02 16:34:35 +0300

    FILTRING

--- src/main/java/com/kurdi/apifiltering/Specifications/ApiSpecifications.java
+++ src/main/java/com/kurdi/apifiltering/Specifications/ApiSpecifications.java
@@ -8,7 +8,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -90,4 +89,7 @@ public class ApiSpecifications<T> {
             }
         };
     }
+
+
+
 }
